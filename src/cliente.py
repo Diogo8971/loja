15b9b187eb9e058a5ff419a2551c51fefae9fc46
Diogo Martins <a89836@ualg.pt>
2025-11-18 15:17:34 +0000

    Adicionei mais funções ao menu do cliente

--- src/cliente.py
+++ src/cliente.py
@@ -1,9 +1,12 @@
 def menu_cliente():
     while True:
         print("\n--- Menu Cliente ---")
-        print("1 - Adicionar produto ao carrinho")
-        print("2 - Filtrar produtos por preço")
-        print("3 - Finalizar compra")
+        print("1 - Ver catálogo")
+        print("2 - Adicionar produto ao carrinho")
+        print("3 - Ver carrinho")
+        print("4 - Remover do carrinho")
+        print("5 - Filtrar produtos por preço")
+        print("6 - Finalizar compra")
         print("0 - Voltar")
         opcao = input("Escolha: ")
 
