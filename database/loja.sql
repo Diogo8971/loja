77a0e0a1728d00ad7485ed5f632e7ed926c90be1
Samuel Gago <a89837@ualg.pt>
2025-11-19 10:41:25 +0000

    Criei a tabela utilizadores

--- database/loja.sql
+++ database/loja.sql
@@ -6,7 +6,12 @@ CREATE TABLE categorias (
     criado_em DATETIME NOT NULL DEFAULT GETDATE()
 );
 
-
+CREATE TABLE utilizadores (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    nome VARCHAR(255),
+    email VARCHAR(255) UNIQUE,
+    password VARCHAR(255)
+);
 
 
 
