5cd6fdbe5fd340585579da6d6dd8e354bfdfb1aa
Rodrigo Dias <rdias.200706@gmail.com>
2025-11-19 10:26:45 +0000

    criei na base de dados a tabela categorias

---
+++ database/loja.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE categorias (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL UNIQUE,
+    descricao VARCHAR(500),
+    criado_em DATETIME NOT NULL DEFAULT GETDATE()
+);
+
+
+
+
+
